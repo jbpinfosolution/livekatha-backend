aa42c5d5a131a7ea145d5d85237ec7fd254c9c49
jitendrasen_09 <jitendersen8959@gmail.com>
2022-11-29 18:26:57 +0530

    third commit

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost:27017/videodata').then(()=>{
+mongoose.connect('mongodb+srv://jbpinfo:kXYPAWblyfGo323q@cluster0.2php3vm.mongodb.net/?retryWrites=true&w=majority').then(()=>{
     console.log("connection successful")
 }).catch((err)=>{
     console.log(" no connection")
