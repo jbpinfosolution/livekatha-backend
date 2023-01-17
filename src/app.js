7ac0bf2691addb20e2315d89e6e45ce10af9034a
jitendrasen_09 <jitendersen8959@gmail.com>
2023-01-17 16:22:13 +0530

    deploye

--- src/app.js
+++ src/app.js
@@ -2,25 +2,23 @@ const express = require("express");
 const path = require("path");
 require("../src/db/connection");
 const app = express();
-var cors = require('cors');
+var cors = require("cors");
 const port = process.env.PORT || 3000;
 const VideoDetails = require("../src/models/video");
-
-
 
 app.use(express.json());
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, '../build')));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.get("/videos", async (req, res) => {
-    try {
-       const getVideos = await VideoDetails.find({});
-      res.status(201).send(getVideos);
-    } catch (e) {
-      res.status(400).send(e);
-    }
-  });
+  try {
+    const getVideos = await VideoDetails.find({});
+    res.status(201).send(getVideos);
+  } catch (e) {
+    res.status(400).send(e);
+  }
+});
 
 app.post("/post", async (req, res) => {
   try {
@@ -30,6 +28,16 @@ app.post("/post", async (req, res) => {
     res.status(201).send(insertVideo);
   } catch (e) {
     res.status(400).send(e);
+  }
+});
+
+app.delete("/delete/:id", async (req, res) => {
+  try {
+    // const _id = req.params.id
+    const deleteVideo = await VideoDetails.deleteOne({ _id:req.params.id });
+    res.send(deleteVideo);
+  } catch (error) {
+    res.status(500).send(error);
   }
 });
 
