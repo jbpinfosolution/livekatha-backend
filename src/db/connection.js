e7e9a81c715da50ff66359f28721cca452456350
jitendrasen_09 <jitendersen8959@gmail.com>
2023-04-14 16:47:10 +0530

    uri-change

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb+srv://jbpinfo:kXYPAWblyfGo323q@cluster0.2php3vm.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+mongoose.connect('mongodb+srv://jbpinfo:M3xO7iOuhTUPQ4WH@cluster0.2php3vm.mongodb.net/?retryWrites=true&w=majority').then(()=>{
     console.log("connection successful")
 }).catch((err)=>{
     console.log(" no connection")
